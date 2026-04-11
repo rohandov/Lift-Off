ce18378b6dbd35f36079ca3a603e5f08bb6f99be
Rohan Vora <47987509+rohandov@users.noreply.github.com>
2026-04-11 13:01:43 -0700

    feat(app): wire main, ProviderScope, and MaterialApp

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:lift_off/core/theme/app_theme.dart';
+import 'package:lift_off/features/library/library_screen.dart';
+
+class LiftOffApp extends StatelessWidget {
+  const LiftOffApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lift Off',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark(),
+      home: const LibraryScreen(),
+    );
+  }
+}
