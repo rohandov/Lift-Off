41488bfe79662257fb8b02fa7f347d4564ab2f5c
Rohan Vora <47987509+rohandov@users.noreply.github.com>
2026-04-11 12:54:44 -0700

    feat(db): define drift tables for exercises, workouts, history

---
+++ lib/core/db/tables.dart
@@ -0,0 +1,40 @@
+import 'package:drift/drift.dart';
+
+class Exercises extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(min: 1, max: 64)();
+  TextColumn get iconName => text()();
+  IntColumn get defaultSets => integer()();
+  TextColumn get defaultReps => text()();
+  BoolColumn get isCustom => boolean().withDefault(const Constant(false))();
+  DateTimeColumn get createdAt =>
+      dateTime().withDefault(currentDateAndTime)();
+}
+
+class Workouts extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  DateTimeColumn get startedAt => dateTime()();
+  DateTimeColumn get finishedAt => dateTime()();
+}
+
+class WorkoutExercises extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get workoutId =>
+      integer().references(Workouts, #id, onDelete: KeyAction.cascade)();
+  IntColumn get exerciseId => integer()
+      .nullable()
+      .references(Exercises, #id, onDelete: KeyAction.setNull)();
+  IntColumn get position => integer()();
+  TextColumn get nameSnapshot => text()();
+  IntColumn get setsSnapshot => integer()();
+  TextColumn get repsSnapshot => text()();
+  BoolColumn get wasCompleted => boolean().withDefault(const Constant(false))();
+}
+
+class Settings extends Table {
+  TextColumn get key => text()();
+  TextColumn get value => text()();
+
+  @override
+  Set<Column> get primaryKey => {key};
+}
